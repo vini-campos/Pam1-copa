a7c158c090b203ca252a8999404bc43bb7c32452
vini-campos <viniciuscampos1311209@gmail.com>
2026-06-10 11:37:05 -0300

    adição de TabNavigator, Stack e arquivos das páginas.

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <ButtonTabs />
+    </NavigationContainer>
+  );
+}
+
+function ButtonTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'Início' }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{ title: 'Perfil' }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function Home() {
+  return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function Profile() {
+  return (
+    <View style={styles.container}>
+      <Text>Perfil filha da putaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
     </View>
   );
 }
@@ -13,7 +66,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
